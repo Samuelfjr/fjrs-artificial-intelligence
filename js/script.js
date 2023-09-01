e5275ba4094cc85e53c10e2e3d0ee95dc3d2a4c2
Samuelfjr <samucaftww@gmail.com>
2023-08-31 21:06:41 -0300

    refactor: Anima Scroll

--- js/script.js
+++ js/script.js
@@ -1,7 +1,10 @@
-const faqs = document.querySelectorAll(".faq-right");
-
-faqs.forEach(faq => {
-    faq.addEventListener("click", () => {
-        faq.classList.toggle("active");
+const intersectObserver = new IntersectionObserver((entries) => {
+    entries.forEach(entry => {
+        console.log(entry);
+        entry.target.classList.toggle("show", entry.isIntersecting);
     })
 })
+
+const elements = document.querySelectorAll(".animate");
+
+elements.forEach(el => intersectObserver.observe(el));
