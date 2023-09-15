dc5bd0996929f4acc0ffbae53313926597591522
Samuelfjr <samucaftww@gmail.com>
2023-09-15 20:25:17 -0300

    feat: Menu Hambúrguer Parcial

--- js/menu.js
+++ js/menu.js
@@ -4,6 +4,9 @@ const menuNav = document.querySelector(".nav-links");
 const menuToggle = menuNav.querySelector(".menu-toggle")
 
 menuToggle.addEventListener("click", () => {
+
+    document.body.style.overflow = show ? "hidden" : "initial"
+
     menuNav.classList.toggle("on", show);
     show = !show;
 });
