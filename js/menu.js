98b4e90f392c10ca73e1217b2443e6a5474d8e07
Samuelfjr <samucaftww@gmail.com>
2023-09-19 18:50:01 -0300

    refactor: Menu Hambúrguer Parcial

--- js/menu.js
+++ js/menu.js
@@ -1,21 +1,7 @@
-// let show = true;
-
-// const menuNav = document.querySelector(".nav-links");
-// const menuToggle = menuNav.querySelector(".menu-toggle")
-
-// menuToggle.addEventListener("click", () => {
-
-//     document.body.style.overflow = show ? "hidden" : "initial"
-
-//     menuNav.classList.toggle("on", show);
-//     show = !show;
-
-// });
-
-const btnMobile = document.querySelector(".menu-toggle");
+const btnMobile = document.getElementById('btn-mobile')
 
 function toggleMenu() {
-    const nav = document.querySelector('.nav-links');
+    const nav = document.getElementById('nav');
 
     nav.classList.toggle("on");
 }
